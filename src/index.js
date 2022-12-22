0b6ebd21332d2e0dca14d0bf99110bb858bd43bf
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2022-12-22 19:24:57 +0500

    Fix review error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-/* eslint-disable */
 import './style.css';
 
-import  getdata  from './module/apireceive';
-import  postdata  from './module/apisend';
+import getdata from './module/apireceive.js';
+import postdata from './module/apisend.js';
+
 const show = document.getElementById('show');
 const refresh = document.getElementById('Refresh');
 const submit = document.getElementById('button');
@@ -10,33 +10,31 @@ const name = document.getElementById('Name');
 const score = document.getElementById('Score');
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/NlvX6MvUBPuR3j2CIhZm/scores/';
 
-
 const display = (data) => {
-    show.innerHTML = '';
-    if(data.length> 0) {
-        data.forEach(element => {
-            show.innerHTML += `
+  show.innerHTML = '';
+  if (data.length > 0) {
+    data.forEach((element) => {
+      show.innerHTML += `
             <p class="para"> <span class="left"> ${element.user} </span> <span class="right"> ${element.score} </span></p>
-            `
-        });
-    } else {
-        show.innerHTML = `<p class="nodata">There is no data to show! Please add a data</p>`;
-    }
-}
+            `;
+    });
+  } else {
+    show.innerHTML = '<p class="nodata">There is no data to show! Please add a data</p>';
+  }
+};
 
-
-refresh.addEventListener('click', async () =>{
-    const data = await getdata(url)
-    display(data)
-} )
+refresh.addEventListener('click', async () => {
+  const data = await getdata(url);
+  display(data);
+});
 
 submit.addEventListener('click', () => {
-    postdata(url,{user: name.value, score: score.value});
-    name.value = '';
-    score.value = '';
-})
+  postdata(url, { user: name.value, score: score.value });
+  name.value = '';
+  score.value = '';
+});
 
-window.onload = async() => {
-    const data = await getdata(url);
-    display(data)
-}
+window.onload = async () => {
+  const data = await getdata(url);
+  display(data);
+};
